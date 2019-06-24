7e311a69babaf4a433defcc847938678bf45e64b
paldhous <peter@peteraldhous.com>
2019-06-24 08:08:23 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# us-land-cover
-Interactive display of 2016 US National Land Cover database from https://www.mrlc.gov/
+# US Land Cover
+
+Interactive display of 2016 US National Land Cover database, from the [Multi-Resolution Land Characteristics](https://www.mrlc.gov) consortium.
+
+The land cover layer divides the lower 48 states into a 30m-resolution grid and assigns a land cover type, [based on satellite data](https://www.sciencedirect.com/science/article/abs/pii/S092427161830251X), to each grid cell. If you zoom in on somewhere you know well, you'll get a sense of how accurate it is.
+
+View the map [here](https://buzzfeednews.github.io/us-land-cover). You can zoom to a desired location using the OpenStreetMap geocoder search box at top left. The land cover layer can be toggled on and off, and you can choose between OpenStreetMap and satellite imagery basemaps.
+
+### Code
+
+The code to make the map, calling in the 2016 NCLD as [WMS tiles](https://www.mrlc.gov/geoserver/mrlc_display/NLCD_2016_Land_Cover_L48/wms?service=WMS&request=GetCapabilities), is in the file `landcover.R`. The file `nlcd_legend.csv` contains the labels and Hex color values for the legend.
+
+
+### Questions/Feedback
+Email the author Peter Aldhous at peter.aldhous@buzzfeed.com.
---
+++ landcover.R
@@ -0,0 +1,58 @@
+# load required packages
+library(leaflet)
+library(leaflet.extras)
+library(readr)
+library(dplyr)
+
+# load legend data and edit
+legend <- read_csv("nlcd_legend.csv") %>%
+  mutate(shapes = "square",
+         sizes = 10,
+         borders = colors)
+
+# function to add a custom legend (adapted from https://stackoverflow.com/questions/52812238/custom-legend-with-r-leaflet-circles-and-squares-in-same-plot-legends)
+addLegendCustom <- function(map, colors, labels, sizes, shapes, borders, opacity = 1){
+  make_shapes <- function(colors, sizes, borders, shapes) {
+    paste0(colors, "; width:", sizes, "px; height:", sizes, "px; border:3px solid ", borders, "; border-radius:", shapes)
+  }
+  make_labels <- function(sizes, labels) {
+    paste0("<div style='display: inline-block;height: ", 
+           sizes, "px;margin-top: 4px;line-height: ", 
+           sizes, "px;'>", labels, "</div>")
+  }
+  legend_colors <- make_shapes(colors, sizes, borders, shapes)
+  legend_labels <- make_labels(sizes, labels)
+  return(addLegend(map, colors = legend_colors, labels = legend_labels, opacity = opacity, position = "bottomright"))
+}
+
+# make leaflet map, bringing in National Land Cover Database from WMS
+leaflet() %>% 
+  addTiles(group="OpenStreetMap") %>%
+  addProviderTiles(providers$Esri.WorldImagery, group="Satellite") %>%
+  setView(-93.65, 42.0285, zoom = 4) %>%
+  addResetMapButton() %>%
+  # add address/location search
+  addSearchOSM(options = searchFeaturesOptions(
+    zoom=15,
+    autoCollapse = FALSE, 
+    hideMarkerOnCollapse = TRUE)) %>%
+  # add land cover layer
+  addWMSTiles(
+    "https://www.mrlc.gov/geoserver/mrlc_display/NLCD_2016_Land_Cover_L48/ows?SERVICE=WMS&",
+    layers = "NLCD_2016_Land_Cover_L48",
+    options = WMSTileOptions(format = "image/png", transparent = TRUE),
+    attribution = "National Land Cover Database 2016",
+    group = "Land cover"
+  ) %>%
+  addLayersControl(
+    baseGroups = c("OpenStreetMap","Satellite"),
+    overlayGroups = "Land cover",
+    options = layersControlOptions(collapsed = FALSE)
+  ) %>%
+  addLegendCustom(legend$colors, legend$labels, legend$sizes, legend$shapes, legend$borders)
+
+
+
+
+
+  
